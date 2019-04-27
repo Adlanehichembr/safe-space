deb664b5469649999068d0f043d53f37df1fdc09
toblich <toblich@gmail.com>
2019-04-27 18:01:01 +0400

    Shape response object in server

--- server/src/index.js
+++ server/src/index.js
@@ -13,8 +13,28 @@ app.get('/ping', (req, res) => {
 });
 
 app.post('/message', async (req, res) => {
-  const msg = await assistant.query(req.body.context || {}, req.body.message);
-  res.json(msg);
+  const {context, output} = await assistant.query(req.body.context || {}, req.body.message);
+
+  const texts = output.generic
+    .filter(o => o.response_type === 'text')
+    .map(o => o.text);
+
+  const buttonOptions = output.generic
+    .filter(o => o.response_type === 'option');
+
+  console.log(buttonOptions)
+
+  const buttons = buttonOptions && buttonOptions.length ?
+    buttonOptions.map(o => o.options.map(p => ({label: p.label, value: p.value.input.text})))[0]
+    : null;
+
+  const image = {
+    link:  'http://open-this-on-click.com',
+    photo: 'http://download-this-for-preview.img',
+    text:  'display this'
+  }; // TODO implement
+
+  res.json({context, texts, buttons, image});
 });
 
 // -- Start server --
